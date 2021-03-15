b58605b46e97f462380173205cc0947f3ab7bd47
nebiros88 <36847332+nebiros88@users.noreply.github.com>
2021-03-15 15:59:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  array = Array.isArray(array) ? array : arguments;
+  if (array.length === 0) return 0;
+  const sorted = [...array].sort((a, b) => a - b);
+  return sorted[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  array = Array.isArray(array) ? array : arguments;
+  if (array.length === 0) return 0;
+  const sorted = [...array].sort((a, b) => a - b);
+  return sorted[sorted.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  array = Array.isArray(array) ? array : arguments;
+  if(array.length === 0) return 0;
+  let sum = 0;
+  array.forEach(el => sum += el);
+  return (sum / array.length);
 }
